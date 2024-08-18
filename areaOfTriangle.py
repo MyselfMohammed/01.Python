7ffc0b1d345bf546577a6ccca65656ad0f41ab2d
MyselfMohammed <myselfmohammedgouse@gmail.com>
2024-08-18 20:30:30 +0530

    Delete areaOfTriangle.py

--- areaOfTriangle.py
+++
@@ -1,8 +0,0 @@
-class areaOfTriangle () :
-
-#print area of triangle using functions
-
-    def areaOfTriangle(Height,Breadth):
-        print("Height :",Height)
-        print("Breadh :",Breadth)
-        print("Area of Triangle :",(Height*Breadth)/2)
